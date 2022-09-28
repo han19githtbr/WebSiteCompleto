a7b2dad5dd59d12b94500e6efcf4b732762a96f6
han19githtbr <milliance23@gmail.com>
2022-09-28 17:12:25 -0300

    orçamento JS

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ var hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 })
+
+document.querySelector("#qty").addEventListener("change", atualizarPreco)
+document.querySelector("#wp").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+} )
+
+function atualizarPreco(){
+    const qty = document.querySelector("#qty").value
+    const temWP = document.querySelector("#wp").checked
+    const incluirLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value  
+    
+    let preco = qty * 100;
+    if (temWP) {
+        preco = preco + (preco * 0.10)
+    }
+    if (incluirLayout) {
+        preco += 500;
+    }
+    
+    let taxaUrgencia = 1 - prazo * 0.1;
+    preco = preco + (preco * taxaUrgencia)
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
